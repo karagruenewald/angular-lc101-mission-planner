0da0dd23526e3a9d9ffc5516241c3765b89cc6a5
Kara Gruenewald <karakilkenny@karas-imac.attlocal.net>
2020-06-15 17:04:19 -0500

    completed part 1

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -21,11 +21,22 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   withinAllowedMass: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
 
-   // Code your addItem function here:
+   addItem(equipment: object) {
+     this.cargoHold.push(equipment);
+     this.cargoMass = this.cargoMass + equipment.mass;
+     if (this.cargoMass >= this.maximumAllowedMass-200) {
+      this.withinAllowedMass = true;
+       return true;
+       
+     } else {
+       return false;
+     }
+   }
    
 }
